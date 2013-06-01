b152e4980a78f9336c4af565b0e8619d19490b78
Clayton Lengel-Zigich <clayton@claytonlz.com>
2013-06-01 10:04:43 -0700

    Rails init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MoarBetterCensusApi::Application.config.secret_key_base = 'ad8e6b207669d06a295e6df1f053ec18ff442b973aa6e66bd7a228f2f438f82edc4793e49e38380b42af31e0364ff19d165fc3213cf796cb90f99d7077e89067'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MoarBetterCensusApi::Application.config.session_store :encrypted_cookie_store, key: '_moar-better-census-api_session'
