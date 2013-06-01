9c812a829b7928fe3ae060878a0a6b4455e17725
Jade Meskill <jade@integrumtech.com>
2013-06-01 10:56:46 -0700

    add rspec and controller tests, add controller to accept api_key

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,20 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :authenticate
+
+  attr_accessor :api_key
+
+  def index
+    head :ok
+  end
+
+  protected
+  def authenticate
+    return if api_key.present?
+    authenticate_or_request_with_http_basic do |username, password|
+      @api_key ||= username
+    end
+  end
 end
---
+++ spec/controllers/application_controller_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe ApplicationController do
+
+  describe '#index' do
+
+    context "no key provided" do
+      before :each do
+        get :index
+      end
+
+      it "should return a 401" do
+        @response.status.should == 401
+      end
+    end
+
+    # context "key provided" do
+    #   before :each do
+    #     @request.env["HTTP_AUTHORIZATION"] = "Basic " + Base64::encode64("some_key:")
+    #   end
+
+    #   it "should return a 200" do
+    #     @response.should == 200
+    #   end
+    # end
+  end
+end
